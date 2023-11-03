297f45fdaf6be89bfa638f3b356e10b5b9b95b6a
Meagan Hollingsworth <mhollingsworth@google.com>
2023-11-03 16:38:52 -0400

    created the user.js file updating the user properties

--- src/User.js
+++ src/User.js
@@ -1,5 +1,23 @@
 class User {
   // User code here
-}
+  constructor(username, password, age, loggedIn) {
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = false;
+  }
+
+  login(password) {
+    if (password != this.password) {
+      throw "incorrect password";
+    }
+
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+};
 
 module.exports = User
