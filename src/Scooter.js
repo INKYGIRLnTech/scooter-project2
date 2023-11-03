f907b7084d456db410407c78e115307b3b9ca049
Meagan Hollingsworth <mhollingsworth@google.com>
2023-11-03 17:01:37 -0400

    created the class: Scooter

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,35 @@
 class Scooter{
   // scooter code here
+  
+  static nextSerial = 0;
+
+  constructor(station) {
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial++;
+    this.charge = 100;
+    this.isBroken = false;
+  }
+
+  rent(user) {
+    if (this.isBroken) {
+      throw "scooter needs repair";
+    } else if (this.charge < 20) {
+      throw "scooter needs to charge";
+    }
+
+    this.user = user;
+    this.station = null;
+  }
+
+  dock(station) {
+    this.station = station;
+    this.user = null;
+  }
+
+  recharge() {
+    this.charge = 100;
+  }
 }
 
 
